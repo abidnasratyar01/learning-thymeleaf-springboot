b9b8c1851d4fa9e144ea627f04675f2887466118
Ahmad Abid <abidnasratyar01@gmail.com>
2023-02-26 15:33:43 +0430

    Added a thymeleaf dynamic data fetch.

--- thymeleafspringboot/src/main/java/com/thymeleaf/springboot/controllers/MyController.java
+++ thymeleafspringboot/src/main/java/com/thymeleaf/springboot/controllers/MyController.java
@@ -1,6 +1,7 @@
 package com.thymeleaf.springboot.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MyController {
 
 	@RequestMapping(value="/test", method=RequestMethod.GET)
-	public String test()
+	public String test(Model model)
 	{
+		model.addAttribute("name", "Ahmad Abid");
+		model.addAttribute("Age", 19);
 		System.out.println("I am inside Test");
 		return "test";
 	}
